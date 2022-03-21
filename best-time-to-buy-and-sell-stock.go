98a288778c7d4ca1c9bb66daa70e3d096e5dc75e
Akanksha kumari <akankshakumari393@gmail.com>
2022-03-21 11:44:29 +0530

    Optimize Time complexity

--- best-time-to-buy-and-sell-stock.go
+++ best-time-to-buy-and-sell-stock.go
@@ -15,3 +15,34 @@ for i:=0; i< len(prices); i++ {
    }
    return maxProfit
 }
+
+// Optimized approach
+
+//Time complexity - O(n)
+//Space complexity - O(n)
+
+// By Traversing Backward get the maximum amount by selling stock on that particular day
+func maxProfit(prices []int) int {
+	var maxProfit int
+	var profit int
+	maxSell := make([]int, len(prices))
+	maxSell[len(prices)-1] = prices[len(prices)-1]
+	for i := len(prices) - 2; i >= 0; i-- {
+
+		if maxSell[i+1] > prices[i] {
+			maxSell[i] = maxSell[i+1]
+		} else {
+			maxSell[i] = prices[i]
+		}
+	}
+	// By now we have a slice maxSell which specify that on a particular day what would be the maximum value of the stock in future if we plan to buy on that day
+	// we now have to calculate the profit and keep the maximum in a variable
+	for i := 0; i < len(prices); i++ {
+		// calculate profit by buying on ith day, we already know the max price of that stock in future
+		profit = maxSell[i] - prices[i]
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+	return maxProfit
+}
