5dcb34f3514598daa52c757cb4326ef0f3379db4
Akanksha kumari <akankshakumari393@gmail.com>
2022-03-21 10:57:30 +0530

    Optimize intersection-of-two-arrays-ii.go

--- intersection-of-two-arrays-ii.go
+++ intersection-of-two-arrays-ii.go
@@ -11,11 +11,12 @@ func intersect(nums1 []int, nums2 []int) []int {
     var ans []int
     array1Content := make(map[int]int)
     for _, num := range nums1{
-        if count, exist := array1Content[num]; exist {
-            array1Content[num] = count +1  
-        } else{
-          array1Content[num] = 1  
-        } 
+//         if count, exist := array1Content[num]; exist {
+//             array1Content[num] = count +1  
+//         } else{
+//           array1Content[num] = 1  
+//         } 
+        array1Content[num] = array1Content[num] +1
     }
     for _, num := range nums2{
         if count, exist := array1Content[num]; exist {
