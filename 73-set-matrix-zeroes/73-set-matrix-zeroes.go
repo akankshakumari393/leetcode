8b2f5d0700cf671b1c317fc2ec799d22d0fe7a7a
Akanksha kumari <akankshakumari393@gmail.com>
2022-06-06 19:52:22 +0530

    Time: 29 ms (8.04%), Space: 6.3 MB (62.41%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.go
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.go
@@ -5,14 +5,14 @@ func setZeroes(matrix [][]int)  {
 	}
 	n := len(matrix[0])
     //using extra constant space
-    rowToSet0 := make([]int,m)
-    columnToSet0 := make([]int,n)
+    clearRow := make([]int,m)
+    clearColumn := make([]int,n)
 
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if matrix[i][j] == 0 {
-                rowToSet0[i] = 1
-                columnToSet0[j] = 1
+                clearRow[i] = 1
+                clearColumn[j] = 1
             }
 		}
 	}
@@ -22,29 +22,49 @@ func setZeroes(matrix [][]int)  {
   //   fmt.Println(columnToSet0)   
   // //  1,2,3
     
-    for j := 0; j < len(columnToSet0); j++ {
-        if columnToSet0[j] == 1 {
-                    for i:=0; i< m;i++{
-            matrix[i][j] = 0
-        }
+//     for j := 0; j < len(clearcolumn); j++ {
+//         if clearColumn[j] == 1 {
+//                     for i:=0; i< m;i++{
+//             matrix[i][j] = 0
+//         }
 
-        } 
-    }
+//         } 
+//     }
 
-	// for i := 0; i < m; i++ {
-	// 	for j := 0; j < n; j++ {
-	// fmt.Print(matrix[i][j])
-	// }
-	// fmt.Println("")
-	// }
+// 	// for i := 0; i < m; i++ {
+// 	// 	for j := 0; j < n; j++ {
+// 	// fmt.Print(matrix[i][j])
+// 	// }
+// 	// fmt.Println("")
+// 	// }
 
     
-    for i := 0; i < len(rowToSet0); i++ {
-        if rowToSet0[i] ==1 {
-        for j:=0; j< n;j++{
-            matrix[i][j] = 0
-        }
-        }
-    }
+//     for i := 0; i < len(clearRow); i++ {
+//         if clearRow[i] ==1 {
+//         for j:=0; j< n;j++{
+//             matrix[i][j] = 0
+//         }
+//         }
+//     }
+    
+    // optimized 
+
+	for col, val := range clearColumn {
+		if val == 1 {
+			for i := 0; i < m; i++ {
+				matrix[i][col] = 0
+			}
+		}
+	}
+	for row, val := range clearRow {
+		if val == 1 {
+			for i := 0; i < n; i++ {
+				matrix[row][i] = 0
+			}
+		}
+	}
+    
+    
+    
     
 }
