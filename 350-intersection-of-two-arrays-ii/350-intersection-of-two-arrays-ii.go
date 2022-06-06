964f4432aefd59cd4d0088571d2c4e2a1f123cd7
Akanksha kumari <akankshakumari393@gmail.com>
2022-06-06 18:54:41 +0530

    Time: 0 ms (100.00%), Space: 3.2 MB (22.67%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.go
@@ -1,10 +1,3 @@
-// Hashing
-//Time Complexity O(n)
-// Space complexity - O(n) It uses a map for first array, and array to store answer  
-// Populate a map which contains the element of first array as key and the count of it a value.
-// Traverse through the second array, if the element exist in Map and it count >0 then add the //number to answer and decrese the count.
-
-
 func intersect(nums1 []int, nums2 []int) []int {
     var ans []int
 
