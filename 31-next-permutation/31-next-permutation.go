826a0ce1577266cb558644e25334c41179eefdf4
Akanksha kumari <akankshakumari393@gmail.com>
2022-06-07 11:02:44 +0530

    Time: 10 ms (8.45%), Space: 2.5 MB (9.33%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.go
@@ -0,0 +1,25 @@
+func nextPermutation(nums []int)  {
+    n := len(nums)
+    localMaximaBreak := -1
+    for i:= n-2; i>=0; i-- {
+        if nums[i] < nums[i+1]{
+            localMaximaBreak = i
+    for i:= n-1; i>=0; i-- {
+        if nums[i] > nums[localMaximaBreak] {
+            nums[i], nums[localMaximaBreak] = nums[localMaximaBreak], nums[i]
+            break
+        }
+    }
+       
+            
+            break
+        }
+    }
+    for i, j :=(localMaximaBreak+1), n-1; i < j; i, j = i+1, j-1 {
+        nums[i], nums[j] = nums[j], nums[i]
+    }
+    
+    
+    
+    
+}
