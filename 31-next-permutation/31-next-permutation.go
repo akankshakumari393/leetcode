b2567b078599191597b1bcc364047bfbff2bbe9b
Akanksha kumari <akankshakumari393@gmail.com>
2022-06-07 11:15:51 +0530

    Time: 8 ms (8.45%), Space: 2.5 MB (9.33%) - LeetHub

--- 31-next-permutation/31-next-permutation.go
+++ 31-next-permutation/31-next-permutation.go
@@ -2,25 +2,28 @@ func nextPermutation(nums []int)  {
     n := len(nums)
     
     localMaximaBreak := -1
-    
+    // O(n)
     for i:= n-2; i>=0; i-- {
         if nums[i] < nums[i+1]{
             localMaximaBreak = i
-            for i:= n-1; i>=localMaximaBreak; i-- {
-                if nums[i] > nums[localMaximaBreak] {
-                    nums[i], nums[localMaximaBreak] = nums[localMaximaBreak], nums[i]
-                    goto out
-                }
+            break
+        }
+    }
+    // o(n)
+    if localMaximaBreak != -1 {
+        for i:= n-1; i>=localMaximaBreak; i-- {
+            if nums[i] > nums[localMaximaBreak] {
+                nums[i], nums[localMaximaBreak] = nums[localMaximaBreak], nums[i]
+                break
             }
         }
     }
-    
-    out:
+    // this is going to take O(n)
     for i, j :=(localMaximaBreak+1), n-1; i < j; i, j = i+1, j-1 {
         nums[i], nums[j] = nums[j], nums[i]
     }
     
     
-    
+    //Time complexity o(1)
     
 }
