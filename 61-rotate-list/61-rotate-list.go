24eaa0a1c50a56c34a2db7225ea8ff15d0744120
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-03 19:06:07 +0530

    Time: 3 ms (65.31%), Space: 2.7 MB (19.59%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.go
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func rotateRight(head *ListNode, k int) *ListNode {
+    if head == nil ||head.Next == nil || k== 0 {
+        return head
+    }
+    fmt.Println(head.Val)
+    // calculate length
+    cur := head
+    count := 1
+    for cur.Next != nil {
+        count++
+        cur = cur.Next
+    }
+    fmt.Println(count)
+    if k >= count {
+        k= k % count
+    }
+    fmt.Println(k)
+    k = count - k
+    
+    // creates a circular linked list
+    cur.Next = head
+
+     for ;k>=1;k-- {
+        cur = cur.Next
+    }
+    head = cur.Next
+    cur.Next = nil
+    cur.Next= nil
+   
+    
+    return head
+    
+}
