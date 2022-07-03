7f2dd8270dcbe3acc393be2438c3c6c75da1bfbf
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-03 21:00:35 +0530

    Time: 6 ms (92.50%), Space: 4.3 MB (59.50%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.go
@@ -0,0 +1,31 @@
+func minPathSum(grid [][]int) int {
+    var ans [][]int
+    m := len(grid)
+    n := len(grid[0])
+    for i:=0; i< m; i++ {
+    ans = append(ans, make([]int, n))
+    }
+    ans[0][0] = grid[0][0]
+    
+    for i:=1; i< m; i++ {
+        ans[i][0] = grid[i][0] + ans[i-1][0]
+    }
+    
+    for j:=1; j< n; j++ {
+        ans[0][j] = grid[0][j] + ans[0][j-1]
+    }
+    
+    
+    for i:= 1; i< m; i++{
+        for j:= 1; j< n; j++ {
+            // if i==0 && j!=0 {
+            //    ans[0][i] = grid[0][j] + ans[0][j-1]
+            // } else if j==0 && i!=0{
+            //     ans[i][0] = grid[i][0] + ans[i-1][0]
+            // } else if i!=0 && j!=0 {
+            ans[i][j] = grid[i][j] + int((math.Min(float64(ans[i-1][j]), float64(ans[i][j-1]))))                
+            // }
+        }
+    }
+    return ans[m-1][n-1]                            
+}
