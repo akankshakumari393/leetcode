11210880c6c73277633d5e3bf780a4cf5ee002a3
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-03 13:30:12 +0530

    Time: 101 ms (8.46%), Space: 10.2 MB (10.85%) - LeetHub

---
+++ 77-combinations/77-combinations.go
@@ -0,0 +1,28 @@
+func combine(n int, k int) [][]int {
+    var result [][]int
+    candidates := []int{}
+	for i := 1; i <= n; i++ {
+		candidates = append(candidates, i)
+	}
+    findCombination([]int{}, n, k, candidates, &result)
+    return result    
+}
+
+// T: O(n!)
+// S: O(n!)
+func findCombination(included []int, n int, k int, left []int, result *[][]int) {
+    if len(included) == k {
+        ans1 := append([]int{}, included...)
+        *result = append(*result, ans1)
+        return
+    }
+    for idx, l := range left{
+        findCombination(
+            append(included, l),
+            n,
+            k,
+            append([]int{}, left[idx+1:]...),
+            result)        
+    }
+    
+}
