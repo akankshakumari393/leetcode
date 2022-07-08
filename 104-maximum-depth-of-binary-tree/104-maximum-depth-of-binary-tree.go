9f1d22da801cfcf2c17a1fce326e5cd5c4208789
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-08 21:31:24 +0530

    Time: 3 ms (87.69%), Space: 4.3 MB (57.73%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.go
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.go
@@ -10,7 +10,6 @@ func maxDepth(root *TreeNode) int {
     if root == nil {
         return 0
     }
-    fmt.Println("finding height for ", root.Val)
     left := maxDepth(root.Left)
     right := maxDepth(root.Right)
     if left > right {
