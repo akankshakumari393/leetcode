e6ee4dccabbd3a44f68bb625a17766c045070787
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-17 23:41:51 +0530

    Time: 234 ms (22.88%), Space: 16.5 MB (64.41%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.go
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.go
@@ -1,5 +1,5 @@
 func findCenter(edges [][]int) int {
-    if edges[0][0] == edges[1][0] ||  edges[0][0] == edges[1][1] {
+    if edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] {
         return edges[0][0]
     }
     return edges[0][1]
