4de3a81f75f5ebec186fd8d00c9885df0017a8f7
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-17 23:41:30 +0530

    Time: 196 ms (45.76%), Space: 16.6 MB (64.41%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.go
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.go
@@ -1,14 +1,6 @@
 func findCenter(edges [][]int) int {
-    count := make(map[int]int, len(edges)+1)
-    for _, edge := range edges {
-        count[edge[0]] = count[edge[0]] + 1
-        if count[edge[0]] == len(edges) {
-            return edge[0]
-        }
-        count[edge[1]] = count[edge[1]] + 1
-        if count[edge[1]] == len(edges) {
-            return edge[1]
-        }
+    if edges[0][0] == edges[1][0] ||  edges[0][0] == edges[1][1] {
+        return edges[0][0]
     }
-    return 0
+    return edges[0][1]
 }
