8995727fbaf1fad3b7e9eacb09c8f96f62d8fb2f
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-19 17:57:52 +0530

    Time: 272 ms (65.79%), Space: 11.6 MB (38.16%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.go
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.go
@@ -9,23 +9,25 @@ func Constructor() FreqStack {
     return FreqStack{
         freqMap : make(map[int]int),
         mapOfStack : make(map[int][]int),
-        maxFreq : 0,
     }
 }
 
 
 func (this *FreqStack) Push(val int)  {
-    this.freqMap[val] = this.freqMap[val] + 1
-    if this.freqMap[val] > this.maxFreq {
-        this.maxFreq = this.freqMap[val]
+    if _ , ok := this.freqMap[val]; ok {
+        this.freqMap[val]++
+    } else {
+        this.freqMap[val] = 1
+    }
+    f := this.freqMap[val]
+    if f > this.maxFreq {
+        this.maxFreq = f
     }
     if _, ok := this.mapOfStack[this.maxFreq]; !ok{
         var stack []int
         this.mapOfStack[this.maxFreq] = stack
     }
-    stack := this.mapOfStack[this.freqMap[val]]
-    stack = append(stack, val)
-    this.mapOfStack[this.freqMap[val]] = stack
+    this.mapOfStack[f] = append(this.mapOfStack[f], val)
 }
 
 
@@ -33,13 +35,11 @@ func (this *FreqStack) Pop() int {
     currentstack := this.mapOfStack[this.maxFreq]
     val := currentstack[len(currentstack)-1]
     currentstack = currentstack[:len(currentstack)-1]
-    this.freqMap[val] = this.freqMap[val] -1    
+    this.freqMap[val]--    
     this.mapOfStack[this.maxFreq] = currentstack
     if len(currentstack) == 0 {
-        this.maxFreq = this.maxFreq-1
-        return val
+        this.maxFreq--
     }
-
     return val
 }
 
