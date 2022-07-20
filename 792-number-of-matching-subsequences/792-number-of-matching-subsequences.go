6a832b9611a2b477ed2317743427d0a90a7ed539
Akanksha kumari <akankshakumari393@gmail.com>
2022-07-20 23:05:10 +0530

    Time: 344 ms (46.48%), Space: 10.4 MB (14.08%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.go
@@ -0,0 +1,39 @@
+func numMatchingSubseq(s string, words []string) int {
+    slice := []byte(s)
+    hashmap := make(map[byte][]int)
+    for i, element := range slice {
+        hashmap[element] = append(hashmap[element], i)
+    }
+
+    count := 0
+    for _, word := range words {
+        prev_pos := -1
+        i := 0
+        for ; i<len(word); i++ {
+            if arr, ok := hashmap[word[i]]; ok {
+                curr_pos := getCurPosGretThanPrev(prev_pos, arr)
+                if curr_pos == -1 {
+                    break
+                }
+                prev_pos = curr_pos            
+            } else {
+                break
+            }
+            
+        }
+        if i == len(word) {
+            count++
+        }
+    }    
+    return count
+}
+
+
+func getCurPosGretThanPrev(prev_pos int, arr []int) int {
+    for _, elem := range arr {
+        if elem > prev_pos {
+            return elem
+        }
+    }
+    return -1
+}
