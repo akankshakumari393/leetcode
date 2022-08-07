7403e14f234aec9d473f919b86993b1ee9504844
Akanksha kumari <akankshakumari393@gmail.com>
2022-08-07 21:21:12 +0530

    Time: 122 ms (86.54%), Space: 14.1 MB (22.12%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.go
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.go
@@ -1,13 +1,13 @@
 func containsNearbyDuplicate(nums []int, k int) bool {
-	mapOfElement := make(map[int]int)
+	mapOfEle := make(map[int]int)
 
 	for i1, n := range nums {
-		if i2, ok := mapOfElement[n]; ok {
+		if i2, ok := mapOfEle[n]; ok {
 			if i1-i2 <= k {
 				return true
 			}
 		}
-		mapOfElement[n] = i1
+		mapOfEle[n] = i1
 	}
 	return false
 }
