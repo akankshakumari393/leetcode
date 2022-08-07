8061788eb85e5dd95b1adda7d9c0be85924bde49
Akanksha kumari <akankshakumari393@gmail.com>
2022-08-07 12:49:55 +0530

    Time: 4 ms (67.13%), Space: 3.1 MB (41.15%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.go
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.go
@@ -9,17 +9,17 @@ func deleteDuplicates(head *ListNode) *ListNode {
     if head == nil {
         return nil
     }
-    lower := head
+    left := head
     right := head
     
     
     for right.Next != nil {
         if right.Next.Val != right.Val {
-            lower.Next = right.Next
-            lower = lower.Next
+            left.Next = right.Next
+            left = left.Next
         }
         right = right.Next
     }
-    lower.Next = nil
+    left.Next = nil
     return head
 }
