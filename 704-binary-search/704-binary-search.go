6f24c199f96f03bde05199219970bebfdc590155
Akanksha kumari <akankshakumari393@gmail.com>
2022-08-28 11:51:41 +0530

    Time: 37 ms (80.76%), Space: 6.7 MB (75.17%) - LeetHub

--- 704-binary-search/704-binary-search.go
+++ 704-binary-search/704-binary-search.go
@@ -1,15 +1,14 @@
 func search(nums []int, target int) int {
-    pivot, left := 0, 0 
-    right := len(nums) - 1
-    for left <= right {
-      pivot = left + (right - left) / 2
-        if (nums[pivot] == target) {
-        return pivot            
-        }
-        if (target < nums[pivot]) {
-        right = pivot - 1            
-        }else {
-        left = pivot + 1            
+    low :=0
+    high := len(nums) - 1
+    for low <= high {
+        mid := low + (high - low)/2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] > target {
+            high = mid -1
+        } else if nums[mid] < target {
+            low = mid + 1
         }
     }
     return -1
