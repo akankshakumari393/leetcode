0bd2cab430345032863c90d12138737882aab8f8
Akanksha kumari <akankshakumari393@gmail.com>
2022-08-28 11:02:11 +0530

    Time: 4 ms (97.22%), Space: 4.6 MB (91.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.go
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.go
@@ -3,33 +3,55 @@ func diagonalSort(mat [][]int) [][]int {
     n:=len(mat[0])
     // col 0
     for i:=0; i<m; i++{
-        Sort(mat, i, 0, m, n)
+        CountSort(mat, i, 0, m, n)
     }
     // row 0
     for j:=1 ;j<n; j++{
-        Sort(mat, 0, j, m, n)
+        CountSort(mat, 0, j, m, n)
     }
     return mat
 }
 
 
 func Sort(mat [][]int, row, col, m, n int) {
-        temp:=make([]int, 0)
-        x:= row
-        y:= col
-        for x<m&&y<n{
-            temp = append(temp, mat[x][y])
+    temp:=make([]int, 0)
+    x:= row
+    y:= col
+    for x<m&&y<n {
+        temp = append(temp, mat[x][y])
+        x++
+        y++
+    }
+    sort.Ints(temp)
+    idx:=0
+    x=row
+    y=col
+    for x<m&&y<n {
+        mat[x][y] = temp[idx]
+        idx++
+        x++
+        y++
+    }    
+}
+
+func CountSort(mat [][]int, row, col, m, n int) {
+    temp:=make([]int, 101)
+    x:= row
+    y:= col
+    for x<m&&y<n{
+        temp[mat[x][y]]++
+        x++
+        y++
+    }
+    x=row
+    y=col
+
+    for i:=1; i<101; i++{
+        for temp[i] > 0 {
+            mat[x][y] = i
+            temp[i]--
             x++
             y++
         }
-        sort.Ints(temp)
-        idx:=0
-        x=row
-        y=col
-        for x<m&&y<n{
-            mat[x][y] = temp[idx]
-            idx++
-            x++
-            y++
-        }    
+    }
 }
