1bb000dc6b985c0bf335335204877b7f2b775159
Akanksha kumari <akankshakumari393@gmail.com>
2022-08-29 20:46:07 +0530

    Time: 6 ms (38.19%), Space: 2.7 MB (100.00%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.go
@@ -0,0 +1,18 @@
+func findPeakElement(nums []int) int {
+    low :=0
+    high := len(nums) - 1
+    n := len(nums)
+    for low <= high {
+        mid := low + (high - low)/2 
+//        fmt.Println("low, high, mid", low, high, mid)
+        // first case if mid is the answer
+        if ((mid == 0 || nums[mid - 1] <= nums[mid]) && (mid == n - 1 || nums[mid + 1] <= nums[mid])) {
+            return mid   
+        } else if (mid > 0 && nums[mid - 1] > nums[mid]) {// move the right pointer
+            high = mid - 1   
+        } else {
+            low = mid + 1; // move the left pointer          
+        }
+    }
+    return -1
+}
