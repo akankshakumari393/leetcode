e4f447c5d2b3b12ad10d5c75b267aadd91570d0a
Akanksha kumari <akankshakumari393@gmail.com>
2022-09-07 22:08:22 +0530

    Time: 10 ms (53.43%), Space: 3.7 MB (52.95%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.go
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.go
@@ -6,46 +6,21 @@
  * }
  */
 func detectCycle(head *ListNode) *ListNode {
-    left := head
-    right := left
-    for right != nil && right.Next != nil {
-        left = left.Next
-        right = right.Next.Next
-        if left == right {
-            left2 := head
+    slow := head
+    fast := slow
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if fast == slow {
+            slow2 := head
     
-            for left2 != left {
-                left = left.Next
-                left2 = left2.Next
+            for slow2 != slow {
+                slow = slow.Next
+                slow2 = slow2.Next
             }
-            return left
+            return slow
         }
     }
-    
-
     return nil
     
 }
-
-func hasCycle(head *ListNode) bool {
-    left := head
-    right := left
-    for right != nil && right.Next != nil {
-        left = left.Next
-        right = right.Next.Next
-        if left == right {
-            return true
-        }
-    }
-    return false
-}
-
-
-//                     if (fast == slow){
-//                         ListNode slow2 = head; 
-//                         while (slow2 != slow){
-//                             slow = slow.next;
-//                             slow2 = slow2.next;
-//                         }
-//                         return slow;
-//                     }
