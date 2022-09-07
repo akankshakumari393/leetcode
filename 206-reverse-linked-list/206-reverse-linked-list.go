cbb0953e71b4945f37103d44ab456828da92e050
Akanksha kumari <akankshakumari393@gmail.com>
2022-09-07 21:03:36 +0530

    Time: 6 ms (23.98%), Space: 2.9 MB (5.33%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.go
+++ 206-reverse-linked-list/206-reverse-linked-list.go
@@ -5,19 +5,32 @@
  *     Next *ListNode
  * }
  */
+// func reverseList(head *ListNode) *ListNode {
+//     if head == nil {
+//         return nil
+//     }
+//     var prev *ListNode // this needs to be nil
+//     curr := head
+//     for curr != nil {
+//         // Store next
+//        next := curr.Next
+//        curr.Next = prev
+//        prev = curr
+//        curr = next
+//     }
+//     head = prev
+//     return head
+// }
+
+//Using reverseLst
 func reverseList(head *ListNode) *ListNode {
-    if head == nil {
-        return nil
+    if head == nil  || head.Next == nil{
+        return head
     }
-    var prev *ListNode // this needs to be nil
-    curr := head
-    for curr != nil {
-        // Store next
-       next := curr.Next
-       curr.Next = prev
-       prev = curr
-       curr = next
-    }
-    head = prev
-    return head
+    
+    newHead := reverseList(head.Next)
+    tmp := head.Next
+    tmp.Next = head
+    head.Next = nil
+    return newHead
 }
