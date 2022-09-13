dc2f5aa8f172840c54a7983070c1c2e333f42284
Akanksha kumari <akankshakumari393@gmail.com>
2022-09-13 16:51:20 +0530

    Time: 7 ms (53.33%), Space: 3.2 MB (86.70%) - LeetHub

--- 39-combination-sum/39-combination-sum.go
+++ 39-combination-sum/39-combination-sum.go
@@ -1,11 +1,13 @@
+// recursion   ---- you would get the answer at the end
 func combinationSum(candidates []int, target int) [][]int {
     var twoD [][]int
     var a []int
-    findCombinations(0, target, a, candidates, &twoD)
+//    findCombinations(0, target, a, candidates, &twoD)
+    backtrack(0, target, a, candidates, &twoD)
     return twoD
 }
 
-
+// Using Recursion
 func findCombinations(pos int, target int, ans []int, candidates []int, finalAns *[][]int) {
     // fmt.Println("making call", pos, target, ans, candidates, finalAns)
     if pos == len(candidates) {
@@ -22,15 +24,34 @@ func findCombinations(pos int, target int, ans []int, candidates []int, finalAns
     // pick the position
     if candidates[pos] <= target {
         // add the element to answer
-    //fmt.Println("Adding element to ans", candidates[pos], pos, target, ans, finalAns)        
-    ans = append(ans, candidates[pos])
-    //fmt.Println("Added element to ans", candidates[pos], pos, target, ans, finalAns)        
-    findCombinations(pos, target-candidates[pos], ans, candidates, finalAns)
-    //remove the element once tracing is done this represent that even after adding the ele
-    //fmt.Println("Removing element from ans", candidates[pos], pos, target, ans, finalAns)        
+        //fmt.Println("Adding element to ans", candidates[pos], pos, target, ans, finalAns)        
+        ans = append(ans, candidates[pos])
+        //fmt.Println("Added element to ans", candidates[pos], pos, target, ans, finalAns)        
+        findCombinations(pos, target-candidates[pos], ans, candidates, finalAns)
+        //remove the element once tracing is done this represent that even after adding the ele
+        //fmt.Println("Removing element from ans", candidates[pos], pos, target, ans, finalAns)        
 
         ans = ans[:len(ans)-1]
     }
     // not pick the position
     findCombinations(pos+1, target, ans, candidates, finalAns)
+}
+
+
+//backtrack --- answer at every node
+func backtrack(pos int, target int, entry []int, candidates []int, res *[][]int) {
+    if target < 0 {
+        return
+    }
+    if target == 0 {
+        cpy := make([]int, len(entry))
+        copy(cpy, entry)
+        *res = append(*res, cpy)        
+    }
+    for i := pos; i < len(candidates); i++ {
+        entry = append(entry, candidates[i])
+        // because the same number could be repeated so use i
+        backtrack(i, target-candidates[i], entry, candidates, res)
+        entry = (entry)[:len(entry)-1]
+    }
 }
